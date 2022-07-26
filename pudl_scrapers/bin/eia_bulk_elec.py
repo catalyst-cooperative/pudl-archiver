67b46a641ab9466542aac57d9dd1b8dfb318fa28
Zane Selvans <zane.selvans@catalyst.coop>
2022-07-26 09:35:54 -0500

    Standardize some packaging and python environment setup
    * Remove Dockerfile & requirements.txt which we aren't using. Should adapt setup from
      the cheshire repo if we want to containerize the scrapers.
    * Update setup.py to specify install_requires.
    * Add a main() function to the eia_bulk_elec script.
    * Add eia_bulk_elec to the installed console script entry points.
    * Update environment.yml to install the repo with pip and specify channels
    * Remove some explicit UTF encoding comments.
    * Add eia_bulk_elec to scrape_everything.sh
    * Added a basic pyproject.toml file to keep pip happy.

--- pudl_scrapers/bin/eia_bulk_elec.py
+++ pudl_scrapers/bin/eia_bulk_elec.py
@@ -13,20 +13,27 @@ logger.setLevel(logging.INFO)
 
 DATA_URL = "https://api.eia.gov/bulk/ELEC.zip"
 
-output_dir = new_output_dir(Path(OUTPUT_DIR) / "eia_bulk_elec")
-out_path = output_dir / "ELEC.zip"
-out_path.parent.mkdir(parents=True, exist_ok=False)
+def main() -> int:
+    """Download EIA bulk electricity data."""
+    output_dir = new_output_dir(Path(OUTPUT_DIR) / "eia_bulk_elec")
+    out_path = output_dir / "ELEC.zip"
+    out_path.parent.mkdir(parents=True, exist_ok=False)
 
-logger.info("Starting download of EIA bulk electricity data.")
-resp = requests.get(DATA_URL, stream=True)
-resp.raise_for_status()
+    logger.info("Starting download of EIA bulk electricity data.")
+    resp = requests.get(DATA_URL, stream=True)
+    resp.raise_for_status()
 
-total_size_in_bytes = int(resp.headers.get("content-length", 0))
-chunk_size = 2**20  # MB
-progress_bar = tqdm(total=total_size_in_bytes, unit="iB", unit_scale=True)
-with open(out_path, "wb") as f:
-    for chunk in resp.iter_content(chunk_size=chunk_size):
-        progress_bar.update(len(chunk))
-        f.write(chunk)
-progress_bar.close()
-logger.info("Completed download of EIA bulk electricity data.")
+    total_size_in_bytes = int(resp.headers.get("content-length", 0))
+    chunk_size = 2**20  # MB
+    progress_bar = tqdm(total=total_size_in_bytes, unit="iB", unit_scale=True)
+    with open(out_path, "wb") as f:
+        for chunk in resp.iter_content(chunk_size=chunk_size):
+            progress_bar.update(len(chunk))
+            f.write(chunk)
+    progress_bar.close()
+    logger.info("Completed download of EIA bulk electricity data.")
+
+    return 0
+
+if __name__ == "__main__":
+    sys.exit(main())
