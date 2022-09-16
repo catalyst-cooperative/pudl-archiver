2702a51ce2ac8e8c1d111f768360413bd1747418
zschira <zasc3143@colorado.edu>
2022-09-16 13:33:06 -0400

    Update scraper to handle segmented RSS feeds

--- src/pudl_scrapers/bin/ferc_xbrl.py
+++ src/pudl_scrapers/bin/ferc_xbrl.py
@@ -1,14 +1,20 @@
 """A command line interface (CLI) to archive data from an RSS feed."""
 import argparse
+import datetime
 import json
 import logging
 import re
+import time
 import zipfile
+from concurrent.futures import ProcessPoolExecutor as Executor
+from enum import Enum
 from pathlib import Path
 
 import feedparser
+import pydantic
 import requests
-from feedparser import FeedParserDict
+from dateutil import rrule
+from pydantic import BaseModel, Field, HttpUrl, root_validator, validator
 
 import pudl_scrapers.settings
 from pudl_scrapers.helpers import new_output_dir
@@ -17,174 +23,254 @@ logger = logging.getLogger(__name__)
 logger.addHandler(logging.StreamHandler())
 logger.setLevel(logging.INFO)
 
-FERC_RSS_LINK = "https://ecollection.ferc.gov/api/rssfeed"
-SUPPORTED_FORMS = [1, 2, 6, 60, 714]
-# Actual link to XBRL filing is only available in inline html
-# This regex pattern will help extract the actual link
+
 XBRL_LINK_PATTERN = re.compile(r'href="(.+\.(xml|xbrl))">(.+(xml|xbrl))<')  # noqa: W605
+"""Regex pattern to extrac link to XBRL filing from inline html contained in RSS feed."""
+
+BASE_RSS_URL = "https://ecollection.ferc.gov/api/rssfeed"
+"""URL to latest RSS feed.
+
+The most recent 650 filings will be contained in this feed. All older filings can
+be found in month specific feeds that can be retrieved by appending a query string
+to this URL to specify the month and year desired.
+"""
+
+Year = pydantic.conint(ge=2014, le=datetime.datetime.today().year)
+"""Constrained pydantic integer type with all years containing XBRL data."""
+
+
+class FercForm(Enum):
+    """Enum containing all supported FERC forms."""
+
+    FORM_1 = "Form 1"
+    FORM_2 = "Form 2"
+    FORM_6 = "Form 6"
+    FORM_60 = "Form 60"
+    FORM_714 = "Form 714"
+
+    def as_int(self):
+        """Convert form to an integer for creating formatted strings."""
+        match self:
+            case FercForm.FORM_1:
+                return 1
+            case FercForm.FORM_2:
+                return 2
+            case FercForm.FORM_6:
+                return 6
+            case FercForm.FORM_60:
+                return 60
+            case FercForm.FORM_714:
+                return 714
+
+    @classmethod
+    def from_int(cls, form_number: int) -> "FercForm":
+        """Create form from an integer."""
+        if form_number not in [1, 2, 6, 60, 714]:
+            raise ValueError(f"{form_number} is not a valid FERC form number")
+
+        return cls(f"Form {form_number}")
+
+
+class FeedEntry(BaseModel):
+    """This is a pydantic model to wrap a parsed entry from the RSS feed.
+
+    This model does not include all fields available in the feed, but everything
+    necessary for the scraping/archiving process.
+    """
+
+    entry_id: str = Field(..., alias="id")
+    title: str
+    download_url: HttpUrl
+    published_parsed: datetime.datetime
+    ferc_formname: FercForm
+    ferc_year: Year
+    ferc_period: str
+
+    @root_validator(pre=True)
+    def extract_url(cls, entry: dict):  # noqa: N805
+        """Get download URL for inline html in feed entry."""
+        link = XBRL_LINK_PATTERN.search(entry["summary_detail"]["value"])
+        entry["download_url"] = link.group(1).replace(" ", "%")
+        return entry
+
+    @validator("published_parsed", pre=True)
+    def parse_timestamp(cls, timestamp: time.struct_time):  # noqa: N805
+        """Parse timestamp to a standard datetime object.
+
+        The published timestamp is only available as a time.struct_time object in the
+        feed. Converting to a datetime object makes it much more usable within the
+        python ecosystem.
+        """
+        return datetime.datetime.fromtimestamp(time.mktime(tuple(timestamp)))
+
+    def __hash__(self):
+        """Implement hash so FeedEntry can be used in a set.
+
+        Entry ID's are unique, so that's all that is needed for the hash.
+        """
+        return hash(f"{self.entry_id}")
+
+
+FormFilings = dict[Year, set[FeedEntry]]
+"""Type alias for a dictionary containing indexed filings for a single FERC form."""
+
+
+def index_available_entries(
+    requested_forms: list[FercForm],
+) -> dict[FercForm, FormFilings]:
+    """Parse all RSS feeds and index the available filings by Form number and year.
+
+    FERC provides an RSS feed for accessing XBRL filings. However, primary RSS feed
+    only contains the latest 650 filings. To access earlier filings, they also
+    provide month specific feeds that contain all filings submitted for a specific
+    month. This class will parse through all of these feeds and create an index of
+    all filings.
+
+    Args:
+        requested_forms: List of requested forms to archive.
+    """
+    allowable_forms = [form.value for form in FercForm]
+
+    # The first month with available filings is October, 2021
+    feed_start_date = datetime.datetime(2021, 10, 1)
+
+    # Get the last day of the previous month at the time of running
+    feed_end_date = datetime.datetime.today().replace(day=1) - datetime.timedelta(
+        days=1
+    )
+
+    # Get a list of all available month specific feeds
+    rss_feeds = [
+        f"{BASE_RSS_URL}?month={dt.month}&year={dt.year}"
+        for dt in rrule.rrule(
+            rrule.MONTHLY, dtstart=feed_start_date, until=feed_end_date
+        )
+    ]
+
+    # Append feed with latest filings
+    rss_feeds.append(BASE_RSS_URL)
+
+    # Create dictionary for mapping filings to form/year
+    indexed_filings = {
+        FercForm.FORM_1: FormFilings(),
+        FercForm.FORM_2: FormFilings(),
+        FercForm.FORM_6: FormFilings(),
+        FercForm.FORM_60: FormFilings(),
+        FercForm.FORM_714: FormFilings(),
+    }
+
+    logger.info("Indexing filings available in all RSS feeds")
+    # Loop through all feeds and index available filings
+    for feed in rss_feeds:
+        logger.info(f"Parsing RSS feed: {feed}")
+        parsed_feed = feedparser.parse(feed)
+
+        for entry in parsed_feed.entries:
+            # Validate FERC form name
+            if entry["ferc_formname"] not in allowable_forms:
+                continue
+
+            # There are a number of test filings in the feed. Skip these
+            if "Test" in entry["title"]:
+                continue
+
+            parsed_entry = FeedEntry(**entry)
+
+            # Get filings specific to FERC form and append new filing
+            indexed_form = indexed_filings[parsed_entry.ferc_formname]
+            if parsed_entry.ferc_year not in indexed_form:
+                indexed_form[parsed_entry.ferc_year] = set()
+
+            indexed_form[parsed_entry.ferc_year].add(parsed_entry)
+
+    # Return indexed filings for all requested forms
+    return {
+        form: filings
+        for form, filings in indexed_filings.items()
+        if form in requested_forms
+    }
+
+
+def archive_form(form: FercForm, indexed_filings: FormFilings):
+    """Create an archive for each year with filings available for the form in question.
+
+    Args:
+        form: Ferc form.
+        indexed_filings: Dictionary mapping available filings to year of filing.
+    """
+    # Get form number as integer
+    form_number = form.as_int()
+    # Create output directory if it doesn't exist
+    output_dir = new_output_dir(
+        Path(pudl_scrapers.settings.OUTPUT_DIR) / f"ferc{form_number}"
+    )
+    if not output_dir.exists():
+        output_dir.mkdir(parents=True)
+
+    # Loop through all years with filings available
+    for year, filings in indexed_filings.items():
+        print("YEAR", year)
+        metadata = {}
+        archive_path = output_dir / f"ferc{form_number}-{year}.zip"
+        logging.info(f"Creating {archive_path} archive.")
+
+        with zipfile.ZipFile(archive_path, "w") as archive:
+            for filing in filings:
+                # Download filing
+                xbrl_file = requests.get(filing.download_url)
+
+                # Add filing metadata
+                filing_name = f"{filing.title}{filing.ferc_period}"
+                if filing_name in metadata:
+                    metadata[filing_name].update({filing.entry_id: filing.json()})
+                else:
+                    metadata[filing_name] = {filing.entry_id: filing.json()}
+
+                # Write to zipfile
+                with archive.open(f"{filing.entry_id}.xbrl", "w") as f:
+                    logger.info(
+                        f"Writing {filing.title} to form {form_number} archive."
+                    )
+                    f.write(xbrl_file.text.encode("utf-8"))
+
+            # Save snapshot of RSS feed
+            with archive.open("rssfeed", "w") as f:
+                logger.info("Writing rss feed metadata to archive.")
+                f.write(json.dumps(metadata).encode("utf-8"))
+
+
+def archive_filings(requested_forms: list[FercForm]):
+    """Create archives for all requested forms."""
+    indexed_filings = index_available_entries(requested_forms)
+    with Executor() as executor:
+        form_archivers = executor.map(
+            archive_form, indexed_filings.keys(), indexed_filings.values()
+        )
+
+        # Loop through archivers to execute in parallel
+        [_ for _ in form_archivers]
 
 
 def parse_main():
     """Process base commands from the CLI."""
     parser = argparse.ArgumentParser(description="Archive filings from RSS feed")
     parser.add_argument(
-        "-r",
-        "--rss-path",
-        default=FERC_RSS_LINK,
-        help=f"Specify path to RSS feed. This can be either a URL or local path (default value is '{FERC_RSS_LINK}').",
-    )
-    parser.add_argument(
-        "-y",
-        "--years",
-        default=2021,
+        "-f",
+        "--form-numbers",
+        default=[1, 2, 6, 60, 714],
         type=int,
         nargs="*",
-        help="Specify a list of years to filter on. The year defines the report year the "
-        "filing pertains to (default value is 'None', which will select all years available).",
-    )
-    parser.add_argument(
-        "-f",
-        "--form-number",
-        default=1,
-        type=int,
-        help=f"Specify form number for filter. Allowable form numbers include: {SUPPORTED_FORMS}.",
-    )
-    parser.add_argument(
-        "-p",
-        "--period",
-        default=None,
-        help="Specify filing period for filter. Filing period defines the quarter the filing pertains "
-        "to and expects a value of 'Q1', 'Q2', 'Q3', or 'Q4' (default value is 'None',"
-        "which will select filings from all quarters).",
+        help="Specify form numbers to archive. Allowable form numbers include: 1, 2, 6, 60, 714. By default, archives will be created for all forms.",
     )
 
     return parser.parse_args()
-
-
-def archive_filings(
-    feed_path: str,
-    form_number: int,
-    filter_years: list[int] | None,
-    output_dir: Path,
-    filter_period: str | None = None,
-):
-    """Download filings and archive in zipfile.
-
-    Args:
-        feed_path: URL or local file path pointing to RSS feed.
-        form_number: Form number for filter.
-        filter_years: Filing year for filter.
-        output_dir: Directory to write archive to.
-        filter_period: Filing period for filter.
-    """
-    rss_feed = feedparser.parse(feed_path)
-
-    # If no filter years are specified archive all years
-    # Only 2021 and 2022 are available at this time
-    if not filter_years:
-        filter_years = [2021, 2022]
-
-    # Loop through requested years and create archive of available filings
-    for year in filter_years:
-        logger.info(f"Creating form {form_number} archive for year {year}")
-        archive_year(rss_feed, form_number, year, output_dir, filter_period)
-
-
-def archive_year(
-    rss_feed: FeedParserDict,
-    form_number: int,
-    filter_year: int,
-    output_dir: Path,
-    filter_period: str | None = None,
-):
-    """Download filings and archive in zipfile.
-
-    Args:
-        rss_feed: Parsed RSS feed with filing metadata.
-        form_number: Form number for filter.
-        filter_year: Filing year for filter.
-        output_dir: Directory to write archive to.
-        filter_period: Filing period for filter.
-    """
-    # Form name for filter
-    form_name = f"Form {form_number}"
-    archive_path = output_dir / f"ferc{form_number}-{filter_year}.zip"
-
-    logger.info(f"Archiving filings in {archive_path}.")
-
-    # Save JSON file with metadata from RSS feed
-    metadata = {}
-
-    # Open zip file for archiving filings
-    with zipfile.ZipFile(archive_path, "w") as archive:
-        # Loop through entries and filter
-        for entry in rss_feed.entries:
-            year = int(entry["ferc_year"])
-
-            # Only filter years if a desired year was specified
-            if filter_year is not None:
-                if year != filter_year:
-                    continue
-
-            # Filter form name
-            if entry["ferc_formname"] != form_name:
-                continue
-
-            # Filter period
-            if filter_period is not None:
-                if entry["ferc_period"] != filter_period:
-                    continue
-
-            # Get link then download filing
-            link = XBRL_LINK_PATTERN.search(entry["summary_detail"]["value"])
-            filing = requests.get(link.group(1))
-
-            # Add filing metadata
-            filing_name = f"{entry['title']}{entry['ferc_period']}"
-            if filing_name in metadata:
-                metadata[filing_name].update({entry["id"]: entry})
-            else:
-                metadata[filing_name] = {entry["id"]: entry}
-
-            # Write to zipfile
-            with archive.open(f"{entry['id']}.xbrl", "w") as f:
-                logger.info(f"Writing {entry['title']} to archive.")
-                f.write(filing.text.encode("utf-8"))
-
-        # Save snapshot of RSS feed
-        with archive.open("rssfeed", "w") as f:
-            logger.info("Writing rss feed metadata to archive.")
-            f.write(json.dumps(metadata).encode("utf-8"))
 
 
 def main():
     """CLI for archiving FERC XBRL filings from RSS feed."""
     args = parse_main()
 
-    # Create output directory if it doesn't exist
-    output_dir = new_output_dir(
-        Path(pudl_scrapers.settings.OUTPUT_DIR) / f"ferc{args.form_number}"
-    )
-    if not output_dir.exists():
-        output_dir.mkdir(parents=True)
+    # Validate form numbers by converting to enum
+    form_numbers = [FercForm.from_int(form_number) for form_number in args.form_numbers]
 
-    # Validate CLI args
-    if args.form_number not in SUPPORTED_FORMS:
-        raise ValueError(f"Form number {args.form_number} is not a valid form.")
-
-    if args.years:
-        # Check if any years are out of range
-        if any([year < 2021 for year in args.years]):
-            raise ValueError("XBRL data is only available for 2021 forward")
-
-    if args.period:
-        if args.period not in ["Q1", "Q2", "Q3", "Q4"]:
-            raise ValueError(f"Invalid filing period: {args.period}")
-
-    archive_filings(
-        feed_path=args.rss_path,
-        form_number=args.form_number,
-        output_dir=output_dir,
-        filter_years=args.years,
-        filter_period=args.period,
-    )
+    archive_filings(form_numbers)
--- tests/ferc_xbrl_test.py
+++ tests/ferc_xbrl_test.py
@@ -1,99 +1,78 @@
 """Test the FERC XBRL data scraping script."""
-import json
 from pathlib import Path
 
 from pudl_scrapers.bin import ferc_xbrl
 
 BASE_PATH = Path(__file__).parent
 
-EXPECTED_METADATA_2021 = {
-    "Example FilerQ4": {
-        "0": {
-            "id": "0",
-            "guidislink": False,
-            "links": [
-                {"rel": "alternate", "type": "text/html", "href": "https://ferc.gov/"}
-            ],
-            "link": "https://ferc.gov/",
-            "title": "Example Filer",
-            "title_detail": {
-                "type": "text/plain",
-                "language": None,
-                "base": "",
-                "value": "Example Filer",
-            },
-            "summary": 'Form 1<br /> Download Files:<br /><ul><li><a href="https://eCollection.ferc.gov/api/DownloadDocument/146659/1?filename=filing_1.xbrl">XBRL_4_389_20220525171041.xbrl</a><br /></li><li><a href="https://eCollection.ferc.gov/api/DownloadDocument/146683/3?filename=XBRL_4_389_20220525171041_91925.html">XBRL_4_389_20220525171041_91925.html</a><br /></li></ul>',
-            "summary_detail": {
-                "type": "text/html",
-                "language": None,
-                "base": "",
-                "value": 'Form 1<br /> Download Files:<br /><ul><li><a href="https://eCollection.ferc.gov/api/DownloadDocument/146659/1?filename=filing_1.xbrl">XBRL_4_389_20220525171041.xbrl</a><br /></li><li><a href="https://eCollection.ferc.gov/api/DownloadDocument/146683/3?filename=XBRL_4_389_20220525171041_91925.html">XBRL_4_389_20220525171041_91925.html</a><br /></li></ul>',
-            },
-            "published": "Fri, 27 May 2022 20:20:21 -0400",
-            "published_parsed": [2022, 5, 28, 0, 20, 21, 5, 148, 0],
-            "updated": "2022-05-25T20:20:21-04:00",
-            "updated_parsed": [2022, 5, 26, 0, 20, 21, 3, 146, 0],
-            "ferc_filingid": "91925",
-            "ferc_cid": "C011405",
-            "ferc_cpacertrequired": "False",
-            "ferc_cpacertuploaded": "False",
-            "ferc_formname": "Form 1",
-            "ferc_period": "Q4",
-            "ferc_privilegedaccessionnumber": "",
-            "ferc_status": "Accepted",
-            "ferc_submittedon": "5/25/2022 8:20:21 PM",
-            "ferc_year": "2021",
-            "ferc_xbrlfile": {
-                "file": "XBRL_4_389_20220525171041_91925.html",
-                "type": "html_rendering",
-                "url": "https://ecollection.ferc.gov/api/DownloadDocument/146683/3?filename=XBRL_4_389_20220525171041_91925.html",
-                "availability": "Public",
-            },
-            "ferc_xbrlfiling": "",
-        },
-        "1": {
-            "id": "1",
-            "guidislink": False,
-            "links": [
-                {"rel": "alternate", "type": "text/html", "href": "https://ferc.gov/"}
-            ],
-            "link": "https://ferc.gov/",
-            "title": "Example Filer",
-            "title_detail": {
-                "type": "text/plain",
-                "language": None,
-                "base": "",
-                "value": "Example Filer",
-            },
-            "summary": 'Form 1<br /> Download Files:<br /><ul><li><a href="https://eCollection.ferc.gov/api/DownloadDocument/146659/1?filename=filing_2.xbrl">XBRL_4_389_20220525171041.xbrl</a><br /></li><li><a href="https://eCollection.ferc.gov/api/DownloadDocument/146683/3?filename=XBRL_4_389_20220525171041_91925.html">XBRL_4_389_20220525171041_91925.html</a><br /></li></ul>',
-            "summary_detail": {
-                "type": "text/html",
-                "language": None,
-                "base": "",
-                "value": 'Form 1<br /> Download Files:<br /><ul><li><a href="https://eCollection.ferc.gov/api/DownloadDocument/146659/1?filename=filing_2.xbrl">XBRL_4_389_20220525171041.xbrl</a><br /></li><li><a href="https://eCollection.ferc.gov/api/DownloadDocument/146683/3?filename=XBRL_4_389_20220525171041_91925.html">XBRL_4_389_20220525171041_91925.html</a><br /></li></ul>',
-            },
-            "published": "Wed, 25 May 2022 20:20:21 -0400",
-            "published_parsed": [2022, 5, 26, 0, 20, 21, 3, 146, 0],
-            "updated": "2022-05-25T20:20:21-04:00",
-            "updated_parsed": [2022, 5, 26, 0, 20, 21, 3, 146, 0],
-            "ferc_filingid": "91925",
-            "ferc_cid": "C011405",
-            "ferc_cpacertrequired": "False",
-            "ferc_cpacertuploaded": "False",
-            "ferc_formname": "Form 1",
-            "ferc_period": "Q4",
-            "ferc_privilegedaccessionnumber": "",
-            "ferc_status": "Accepted",
-            "ferc_submittedon": "5/25/2022 8:20:21 PM",
-            "ferc_year": "2021",
-            "ferc_xbrlfile": {
-                "file": "XBRL_4_389_20220525171041_91925.html",
-                "type": "html_rendering",
-                "url": "https://ecollection.ferc.gov/api/DownloadDocument/146683/3?filename=XBRL_4_389_20220525171041_91925.html",
-                "availability": "Public",
-            },
-            "ferc_xbrlfiling": "",
-        },
+FORM1_FILINGS = {
+    2021: {
+        ferc_xbrl.FeedEntry(
+            **{
+                "id": "0",
+                "title": "Example Filer",
+                "summary_detail": {
+                    "type": "text/html",
+                    "language": None,
+                    "base": "",
+                    "value": 'Form 1<br /> Download Files:<br /><ul><li><a href="https://eCollection.ferc.gov/api/DownloadDocument/146659/1?filename=filing_0.xbrl">XBRL_4_389_20220525171041.xbrl</a><br /></li><li><a href="https://eCollection.ferc.gov/api/DownloadDocument/146683/3?filename=XBRL_4_389_20220525171041_91925.html">XBRL_4_389_20220525171041_91925.html</a><br /></li></ul>',
+                },
+                "ferc_formname": "Form 1",
+                "published_parsed": [2022, 5, 28, 0, 20, 21, 5, 148, 0],
+                "ferc_period": "Q4",
+                "ferc_year": "2021",
+            }
+        ),
+        ferc_xbrl.FeedEntry(
+            **{
+                "id": "1",
+                "title": "Example Filer",
+                "summary_detail": {
+                    "type": "text/html",
+                    "language": None,
+                    "base": "",
+                    "value": 'Form 1<br /> Download Files:<br /><ul><li><a href="https://eCollection.ferc.gov/api/DownloadDocument/146659/1?filename=filing_1.xbrl">XBRL_4_389_20220525171041.xbrl</a><br /></li><li><a href="https://eCollection.ferc.gov/api/DownloadDocument/146683/3?filename=XBRL_4_389_20220525171041_91925.html">XBRL_4_389_20220525171041_91925.html</a><br /></li></ul>',
+                },
+                "published_parsed": [2022, 3, 28, 0, 20, 21, 5, 148, 0],
+                "ferc_formname": "Form 1",
+                "ferc_period": "Q4",
+                "ferc_year": "2021",
+            }
+        ),
+        ferc_xbrl.FeedEntry(
+            **{
+                "id": "2",
+                "title": "Example Filer 2",
+                "summary_detail": {
+                    "type": "text/html",
+                    "language": None,
+                    "base": "",
+                    "value": 'Form 1<br /> Download Files:<br /><ul><li><a href="https://eCollection.ferc.gov/api/DownloadDocument/146659/1?filename=filing_2.xbrl">XBRL_4_389_20220525171041.xbrl</a><br /></li><li><a href="https://eCollection.ferc.gov/api/DownloadDocument/146683/3?filename=XBRL_4_389_20220525171041_91925.html">XBRL_4_389_20220525171041_91925.html</a><br /></li></ul>',
+                },
+                "published_parsed": [2022, 3, 28, 0, 20, 21, 5, 148, 0],
+                "ferc_formname": "Form 1",
+                "ferc_period": "Q4",
+                "ferc_year": "2021",
+            }
+        ),
+    },
+    2022: {
+        ferc_xbrl.FeedEntry(
+            **{
+                "id": "3",
+                "title": "Example Filer",
+                "summary_detail": {
+                    "type": "text/html",
+                    "language": None,
+                    "base": "",
+                    "value": 'Form 1<br /> Download Files:<br /><ul><li><a href="https://eCollection.ferc.gov/api/DownloadDocument/146659/1?filename=filing_3.xbrl">XBRL_4_389_20220525171041.xbrl</a><br /></li><li><a href="https://eCollection.ferc.gov/api/DownloadDocument/146683/3?filename=XBRL_4_389_20220525171041_91925.html">XBRL_4_389_20220525171041_91925.html</a><br /></li></ul>',
+                },
+                "published_parsed": [2022, 3, 28, 0, 20, 21, 5, 148, 0],
+                "ferc_formname": "Form 1",
+                "ferc_period": "Q4",
+                "ferc_year": "2022",
+            }
+        ),
     },
 }
 
@@ -104,15 +83,18 @@ def test_archive_filings(mocker):
     zipfile_mock = mocker.MagicMock(name="zipfile")
     mocker.patch("pudl_scrapers.bin.ferc_xbrl.zipfile", new=zipfile_mock)
 
+    new_output_dir_mock = mocker.MagicMock(
+        name="new_output_dir", return_value=Path("./")
+    )
+    mocker.patch("pudl_scrapers.bin.ferc_xbrl.new_output_dir", new=new_output_dir_mock)
+
     # Mock out requests to avoid making any real requests
-    _ = mocker.Mock("pudl_scrapers.bin.requests")
+    _ = mocker.Mock("pudl_scrapers.bin.ferc_xbrl.requests")
 
     # Call function
-    ferc_xbrl.archive_filings(
-        feed_path=BASE_PATH / "data/ferc_rssfeed.atom",
-        form_number=1,
-        filter_years=[2021, 2022],
-        output_dir=Path("./"),
+    ferc_xbrl.archive_form(
+        form=ferc_xbrl.FercForm.FORM_1,
+        indexed_filings=FORM1_FILINGS,
     )
 
     # Test that zipfile was created with proper name
@@ -123,17 +105,12 @@ def test_archive_filings(mocker):
 
     # Test that all expected filings were written to zip
     archive_mock.open.assert_any_call("0.xbrl", "w")
-    archive_mock.open.assert_any_call("1.xbrl", "w")
+    archive_mock.open.assert_any_call("2.xbrl", "w")
     archive_mock.open.assert_any_call("rssfeed", "w")
-
-    # Test metadata was written as expected
-    archive_mock.open.return_value.__enter__.return_value.write.assert_any_call(
-        json.dumps(EXPECTED_METADATA_2021).encode("utf-8")
-    )
 
     # Test that zipfile was created with proper name
     zipfile_mock.ZipFile.assert_any_call(Path("./") / "ferc1-2022.zip", "w")
 
     # Test that all expected filings were written to zip
-    archive_mock.open.assert_any_call("2.xbrl", "w")
+    archive_mock.open.assert_any_call("3.xbrl", "w")
     archive_mock.open.assert_any_call("rssfeed", "w")
