2144ecc9b5af7ee2f1a00f507a718b57a9828527
zschira <zach.schira@catalyst.coop>
2023-05-05 10:44:16 -0400

    Point crosswalk archive at our mirror with 2021 outputs

--- src/pudl_archiver/archivers/epacamd_eia.py
+++ src/pudl_archiver/archivers/epacamd_eia.py
@@ -19,9 +19,7 @@ class EpaCamdEiaArchiver(AbstractDatasetArchiver):
 
     async def get_crosswalk_zip(self) -> tuple[Path, dict]:
         """Download entire repo as a zipfile from github."""
-        url = (
-            "https://github.com/USEPA/camd-eia-crosswalk/archive/refs/heads/master.zip"
-        )
+        url = "https://github.com/catalyst-cooperative/camd-eia-crosswalk-2021/archive/refs/heads/main.zip"
         download_path = self.download_directory / "epacamd_eia.zip"
         await self.download_zipfile(url, download_path)
 
