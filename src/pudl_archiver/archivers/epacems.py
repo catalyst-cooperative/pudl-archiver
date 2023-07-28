718efeececdd8519951af27a09886954a521af99
thinky <ella.belfer@catalyst.coop>
2023-07-28 09:41:53 -0400

    Very WIP call to API

--- src/pudl_archiver/archivers/epacems.py
+++ src/pudl_archiver/archivers/epacems.py
@@ -1,7 +1,10 @@
 """Download EPACEMS data."""
+import json
 import logging
 import os
 from pathlib import Path
+
+import requests
 
 from pudl_archiver.archivers.classes import (
     AbstractDatasetArchiver,
@@ -64,8 +67,7 @@ STATE_ABBREVIATIONS = [
     "wi",
     "wy",
 ]
-
-BASE_URL = "https://api.epa.gov/easey/bulk-files/emissions/hourly/state/"
+BASE_URL = "https://api.epa.gov/easey/bulk-files/"
 parameters = {"api_key": os.environ["EPACEMS_API_KEY"]}  # Set to API key
 
 
@@ -76,15 +78,32 @@ class EpaCemsArchiver(AbstractDatasetArchiver):
 
     async def get_resources(self) -> ArchiveAwaitable:
         """Download EIA bulk electricity resources."""
-        for state in STATE_ABBREVIATIONS:
-            for year in range(1995, 2023):
-                yield self.get_state_year_resource(year=year, state=state)
+        file_list = requests.get(
+            "https://api.epa.gov/easey/camd-services/bulk-files",
+            params=parameters,
+            timeout=5,
+        )  # FIX ME to use existing classes
+        resjson = file_list.content.decode("utf8").replace("'", '"')
+        bulk_files = json.loads(resjson)
+        hourly_emissions_files = [
+            file
+            for file in bulk_files
+            if (file["metadata"]["dataType"] == "Emissions")
+            and (file["metadata"]["dataSubType"] == "Hourly")
+        ]
+        for file in hourly_emissions_files:
+            if "stateCode" in file["metadata"].keys():
+                url = BASE_URL + file["s3Path"]
+                year = file["metadata"]["year"]
+                state = file["metadata"]["stateCode"].lower()
+                # logger.info(url)
+                yield self.get_state_year_resource(year=year, state=state, url=url)
 
-    async def get_state_year_resource(self, year: int, state: str) -> tuple[Path, dict]:
+    async def get_state_year_resource(
+        self, year: int, state: str, url: str
+    ) -> tuple[Path, dict]:
         """Download all available data for a single state/year."""
-        logger.info(f"Downloading EPACEMS data for {state.capitalize()}, {year}")
-
-        url = f"{BASE_URL}/emissions-hourly-{year}-{state}.csv"
+        logger.info(f"Downloading EPACEMS data for {state.upper()}, {year}")
         download_path = self.download_directory / f"epacems-{year}-{state}.csv"
 
         await self.download_file(url, download_path)
