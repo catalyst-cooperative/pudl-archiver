f02ac1b9b98bfe118f551530a1a58c3bf0ec55e2
Christina Gosnell <cgosnell@catalyst.coop>
2023-12-12 14:02:42 -0500

    add TODO for updating the CEMS file names

--- src/pudl_archiver/archivers/epacems.py
+++ src/pudl_archiver/archivers/epacems.py
@@ -73,8 +73,10 @@ class EpaCemsArchiver(AbstractDatasetArchiver):
 
         # Create zipfile to store year/quarter combinations of files
         filename = f"epacems-{year}-{quarter}.csv"
-        # filename = f"epacems-{year}q{quarter}.csv"
         archive_path = self.download_directory / f"epacems-{year}-{quarter}.zip"
+        # TODO: When we are ready to make a new archive, we want to transition to
+        # using this file formart so its more in line with the partition.
+        # filename = f"epacems-{year}q{quarter}.csv"
         # archive_path = self.download_directory / f"epacems-{year}q{quarter}.zip"
         # Override the default asyncio timeout to 14 minutes, just under the API limit.
         await self.download_and_zip_file(
