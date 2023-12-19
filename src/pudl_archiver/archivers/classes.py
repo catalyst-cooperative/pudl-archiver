1fa4368ea1319ff47b5132a652e012686b4742b9
Dazhong Xia <dazhong.xia@catalyst.coop>
2023-12-19 13:38:15 -0500

    Archive quarterly CEMS data in Annual files (#237)
    * Introduce types for the API response
    
    * Put four quarterly files into one annual archive.
    
    * PR fixes: partition format, types, and typos

--- src/pudl_archiver/archivers/classes.py
+++ src/pudl_archiver/archivers/classes.py
@@ -24,7 +24,7 @@ MEDIA_TYPES: dict[str, str] = {
     "csv": "text/csv",
 }
 
-ArchiveAwaitable = typing.Generator[typing.Awaitable[ResourceInfo], None, None]
+ArchiveAwaitable = typing.AsyncGenerator[typing.Awaitable[ResourceInfo], None]
 """Return type of method get_resources.
 
 The method get_resources should yield an awaitable that returns a ResourceInfo named tuple.
@@ -98,7 +98,7 @@ class AbstractDatasetArchiver(ABC):
         self.logger.info(f"Archiving {self.name}")
 
     @abstractmethod
-    async def get_resources(self) -> ArchiveAwaitable:
+    def get_resources(self) -> ArchiveAwaitable:
         """Abstract method that each data source must implement to download all resources.
 
         This method should be a generator that yields awaitable objects that will download
@@ -106,6 +106,18 @@ class AbstractDatasetArchiver(ABC):
         partitions. What this means in practice is calling an `async` function and yielding
         the results without awaiting them. This allows the base class to gather all of these
         awaitables and download the resources concurrently.
+
+        While this method is defined without the `async` keyword, the
+        overriding methods should be `async`.
+
+        This is because, if there's no `yield` in the method body, static type
+        analysis doesn't know that `async def ...` should return
+        `Generator[Awaitable[ResourceInfo],...]` vs.
+        `Coroutine[Generator[Awaitable[ResourceInfo]],...]`
+
+        See
+        https://stackoverflow.com/questions/68905848/how-to-correctly-specify-type-hints-with-asyncgenerator-and-asynccontextmanager
+        for details.
         """
         ...
 
--- src/pudl_archiver/archivers/epacems.py
+++ src/pudl_archiver/archivers/epacems.py
@@ -1,10 +1,15 @@
 """Download EPACEMS data."""
+import datetime
 import json
 import logging
 import os
-from pathlib import Path
+import zipfile
+from collections.abc import Iterable
+from itertools import groupby
 
 import requests
+from pydantic import BaseModel, ValidationError
+from pydantic.alias_generators import to_camel
 
 from pudl_archiver.archivers.classes import (
     AbstractDatasetArchiver,
@@ -13,6 +18,38 @@ from pudl_archiver.archivers.classes import (
 )
 
 logger = logging.getLogger(f"catalystcoop.{__name__}")
+
+
+class BulkFile(BaseModel):
+    """Data transfer object from EPA.
+
+    See https://www.epa.gov/power-sector/cam-api-portal#/swagger/camd-services
+    for details.
+    """
+
+    class Metadata(BaseModel):
+        """Metadata about a specific file."""
+
+        year: int
+        quarter: int
+        data_type: str
+        data_sub_type: str
+
+        class Config:  # noqa: D106
+            alias_generator = to_camel
+            populate_by_name = True
+
+    filename: str
+    s3_path: str
+    bytes: int  # noqa: A003
+    mega_bytes: float
+    giga_bytes: float
+    last_updated: datetime.datetime
+    metadata: Metadata
+
+    class Config:  # noqa: D106
+        alias_generator = to_camel
+        populate_by_name = True
 
 
 class EpaCemsArchiver(AbstractDatasetArchiver):
@@ -24,6 +61,16 @@ class EpaCemsArchiver(AbstractDatasetArchiver):
     base_url = "https://api.epa.gov/easey/bulk-files/"
     parameters = {"api_key": os.environ["EPACEMS_API_KEY"]}  # Set to API key
 
+    def __filter_for_complete_metadata(
+        self, files_responses: list[dict]
+    ) -> Iterable[BulkFile]:
+        """Silently drop files that don't have year/data-subtype/etc."""
+        for f in files_responses:
+            try:
+                yield BulkFile(**f)
+            except ValidationError:
+                continue
+
     async def get_resources(self) -> ArchiveAwaitable:
         """Download EPA CEMS resources."""
         file_list = requests.get(
@@ -31,61 +78,67 @@ class EpaCemsArchiver(AbstractDatasetArchiver):
             params=self.parameters,
             timeout=300,
         )
-        if file_list.status_code == 200:
-            resjson = file_list.content.decode("utf8").replace("'", '"')
-            file_list.close()  # Close connection.
-            bulk_files = json.loads(resjson)
-            quarterly_emissions_files = [
-                file
-                for file in bulk_files
-                if (file["metadata"]["dataType"] == "Emissions")
-                and (file["metadata"]["dataSubType"] == "Hourly")
-                and ("quarter" in file["metadata"])
-                and (self.valid_year(file["metadata"]["year"]))
-            ]
-            logger.info(f"Downloading {len(quarterly_emissions_files)} total files.")
-            logger.debug(f"File info: {quarterly_emissions_files}")
-            for i, cems_file in enumerate(quarterly_emissions_files):
-                yield self.get_year_quarter_resource(
-                    file=cems_file, request_count=i + 1
-                )  # Count files starting at 1 for human legibility.
-        else:
+        if file_list.status_code != 200:
             raise AssertionError(
                 f"EPACEMS API request did not succeed: {file_list.status_code}"
             )
+        resjson = file_list.content.decode("utf8").replace("'", '"')
+        file_list.close()  # Close connection.
+        bulk_files = self.__filter_for_complete_metadata(json.loads(resjson))
+        quarterly_emissions_files = [
+            file
+            for file in bulk_files
+            if (file.metadata.data_type == "Emissions")
+            and (file.metadata.data_sub_type == "Hourly")
+            and (file.metadata.quarter in {1, 2, 3, 4})
+            and self.valid_year(file.metadata.year)
+        ]
+        logger.info(f"Downloading {len(quarterly_emissions_files)} total files.")
+        logger.debug(f"File info: {quarterly_emissions_files}")
+        files_by_year = groupby(
+            sorted(quarterly_emissions_files, key=lambda bf: bf.metadata.year),
+            lambda bf: bf.metadata.year,
+        )
+        for year, files in files_by_year:
+            yield self.get_year_resource(year, list(files))
 
-    async def get_year_quarter_resource(
-        self, file: dict[str, str | dict[str, str]], request_count: int | None
-    ) -> tuple[Path, dict]:
-        """Download all available data for a single quarter in a year.
+    async def get_year_resource(
+        self, year: int, files: Iterable[BulkFile]
+    ) -> ResourceInfo:
+        """Download all available data for a year.
 
         Args:
-            file: a dictionary containing file characteristics from the EPA API.
-                See https://www.epa.gov/power-sector/cam-api-portal#/swagger/camd-services
-                for expected format of dictionary.
-            request_count: the number of the request.
+            year: the year we're downloading data for
+            files: the files we've associated with this year.
         """
-        url = self.base_url + file["s3Path"]
-        year = file["metadata"]["year"]
-        quarter = file["metadata"]["quarter"]
+        archive_path = self.download_directory / f"epacems-{year}.zip"
+        for file in files:
+            url = self.base_url + file.s3_path
+            quarter = file.metadata.quarter
 
-        # Useful to debug at download time-outs.
-        logger.info(f"Downloading {year} Q{quarter} EPACEMS data from {url}.")
+            # Useful to debug at download time-outs.
+            logger.info(f"Downloading {year} Q{quarter} EPACEMS data from {url}.")
 
-        # Create zipfile to store year/quarter combinations of files
-        filename = f"epacems-{year}-{quarter}.csv"
-        archive_path = self.download_directory / f"epacems-{year}-{quarter}.zip"
-        # TODO: When we are ready to make a new archive, we want to transition to
-        # using this file formart so its more in line with the partition.
-        # filename = f"epacems-{year}q{quarter}.csv"
-        # archive_path = self.download_directory / f"epacems-{year}q{quarter}.zip"
-        # Override the default asyncio timeout to 14 minutes, just under the API limit.
-        await self.download_and_zip_file(
-            url=url, filename=filename, archive_path=archive_path, timeout=60 * 14
-        )
-        logger.info(  # Verbose but helpful to track progress
-            f"File no. {request_count}: Downloaded {year} Q{quarter} EPA CEMS hourly emissions data."
-        )
+            filename = f"epacems-{year}q{quarter}.csv"
+            file_path = self.download_directory / filename
+            await self.download_file(url=url, file=file_path, timeout=60 * 14)
+
+            with zipfile.ZipFile(
+                archive_path,
+                "a",
+                compression=zipfile.ZIP_DEFLATED,
+            ) as archive, file_path.open("rb") as f:
+                archive.writestr(filename, f.read())
+
+            # Don't want to leave multiple giant CSVs on disk, so delete
+            # immediately after they're safely stored in the ZIP
+            file_path.unlink()
+
         return ResourceInfo(
-            local_path=archive_path, partitions={"year_quarter": f"{year}q{quarter}"}
+            local_path=archive_path,
+            partitions={
+                "year_quarter": sorted(
+                    [f"{year}q{file.metadata.quarter}" for file in files]
+                ),
+            },
         )
