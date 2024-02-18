6f1be7802def4880b16ae108d2ed7016909b2838
zschira <zach.schira@catalyst.coop>
2024-02-18 10:52:32 -0500

    Split sec archives to year-quarter

--- src/pudl_archiver/archivers/sec10k.py
+++ src/pudl_archiver/archivers/sec10k.py
@@ -12,6 +12,7 @@ from pudl_archiver.archivers.classes import (
     ArchiveAwaitable,
     ResourceInfo,
 )
+from pudl_archiver.frictionless import ZipLayout
 from pudl_archiver.utils import rate_limit_tasks
 
 logger = logging.getLogger(f"catalystcoop.{__name__}")
@@ -25,26 +26,33 @@ class Sec10KArchiver(AbstractDatasetArchiver):
     name = "sec10k"
     concurrency_limit = 1
     resumeable = True
+    fail_on_file_size_change = False
+    fail_on_dataset_size_change = False
 
     async def get_resources(
         self,
     ) -> ArchiveAwaitable:
         """Download SEC 10-K resources."""
         for year in range(1993, 2024):
-            if any([str(year) in fname for fname in self.existing_files]):  # noqa: C419
-                logger.info(f"Skipping {year}, archive already exists in deposition.")
-            else:
-                yield self.get_year_resource(year)
+            for quarter in range(1, 5):
+                if any([f"{year}q{quarter}" in fname for fname in self.existing_files]):  # noqa: C419
+                    logger.info(
+                        f"Skipping {year}, archive already exists in deposition."
+                    )
+                    continue
 
-    async def get_year_resource(self, year: int) -> ResourceInfo:
+                if year > 1993 or quarter > 3:
+                    yield self.get_year_quarter(year, quarter)
+
+    async def get_year_quarter(self, year: int, quarter: int) -> ResourceInfo:
         """Download and zip all filings for a given year."""
         logger.info(f"Downloading files from {year}")
 
         # Wait one second before starting next year to clear rate limit
         await asyncio.sleep(1)
 
-        year_index = await self.get_year_index(year)
-        form_index = year_index[year_index["Form Type"].str.startswith("10-K")]
+        quarter_index = await self.get_quarter_index(year, quarter)
+        form_index = quarter_index[quarter_index["Form Type"].str.startswith("10-K")]
 
         # Wait one second before starting next year to clear rate limit
         await asyncio.sleep(1)
@@ -57,40 +65,42 @@ class Sec10KArchiver(AbstractDatasetArchiver):
                 self.download_file(url, buffer, headers=USER_AGENT_HEADER)
             )
 
-        year_archive = self.download_directory / f"sec10k-{year}.zip"
+        year_archive = self.download_directory / f"sec10k-{year}q{quarter}.zip"
+        zip_files = set()
         async for url, buffer in rate_limit_tasks(download_tasks, rate_limit=8):
             logger.info(f"Downloaded: {url}")
             fname = url.replace(f"{BASE_URL}/", "")
+            buffer.seek(0)
             self.add_to_archive(year_archive, fname, buffer)
+            zip_files.add(fname)
 
         # Add index containing filing metadata to archive
         index_buffer = io.BytesIO()
         form_index.to_csv(index_buffer)
         self.add_to_archive(year_archive, "index.csv", index_buffer)
 
-        return ResourceInfo(local_path=year_archive, partitions={"year": year})
+        return ResourceInfo(
+            local_path=year_archive,
+            partitions={"year_quarter": f"{year}q{quarter}"},
+            layout=ZipLayout(file_paths=zip_files),
+        )
 
-    async def get_year_index(self, year: int) -> pd.DataFrame:
+    async def get_quarter_index(self, year: int, quarter: int) -> pd.DataFrame:
         """Download index file for each quarter in year and return DataFrame of index."""
         # Header is row 9 and row 10 is a delimiter
         skiprows = [0, 1, 2, 3, 4, 5, 6, 7, 8, 10]
-        qtr_indices = []
-        for qtr in range(1, 5):
-            logger.info(f"Getting index for q{qtr}")
-            index_file = self.download_directory / "index.gz"
-            await self.download_file(
-                f"{BASE_URL}/edgar/full-index/{year}/QTR{qtr}/master.gz",
-                index_file,
-                headers=USER_AGENT_HEADER,
-            )
+        index_file = self.download_directory / "index.gz"
+        await self.download_file(
+            f"{BASE_URL}/edgar/full-index/{year}/QTR{quarter}/master.gz",
+            index_file,
+            headers=USER_AGENT_HEADER,
+        )
 
-            try:
-                with gzip.open(index_file, mode="rt") as f:
-                    df = pd.read_csv(f, sep="|", skiprows=skiprows)
-            except UnicodeDecodeError:
-                with gzip.open(index_file, mode="rt", encoding="latin-1") as f:
-                    df = pd.read_csv(f, sep="|", skiprows=skiprows)
-            df["quarter"] = qtr
-            qtr_indices.append(df)
+        try:
+            with gzip.open(index_file, mode="rt") as f:
+                df = pd.read_csv(f, sep="|", skiprows=skiprows)
+        except UnicodeDecodeError:
+            with gzip.open(index_file, mode="rt", encoding="latin-1") as f:
+                df = pd.read_csv(f, sep="|", skiprows=skiprows)
 
-        return pd.concat(qtr_indices)
+        return df
