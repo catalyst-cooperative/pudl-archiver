4502f8adf5a03689348e7eaf19128e0948599ce2
zschira <zach.schira@catalyst.coop>
2024-04-08 12:12:36 -0400

    Actually fix zenodo api client init

--- src/pudl_archiver/depositors/zenodo/depositor.py
+++ src/pudl_archiver/depositors/zenodo/depositor.py
@@ -122,7 +122,7 @@ class ZenodoAPIClient(BaseModel):
         cls,
         dataset: str,
         session: aiohttp.ClientSession,
-        run_settings: RunSettings,
+        sandbox: bool,
     ) -> "ZenodoAPIClient":
         """Initialize API client connection.
 
@@ -131,7 +131,7 @@ class ZenodoAPIClient(BaseModel):
             session: HTTP handler - we don't use it directly, it's wrapped in self._request.
             run_settings: Settings from CLI.
         """
-        self = cls(dataset_id=dataset, sandbox=run_settings.sandbox)
+        self = cls(dataset_id=dataset, sandbox=sandbox)
         self._session = session
         self._request = self._make_requester(session)
         self._dataset_settings_path = (
