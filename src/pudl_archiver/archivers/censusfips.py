3df72cff38294f831e7640719d6e4d28e2296b7e
Christina Gosnell <cgosnell@catalyst.coop>
2025-01-09 12:50:14 -0500

    add in the 2000 txt file

--- src/pudl_archiver/archivers/censusfips.py
+++ src/pudl_archiver/archivers/censusfips.py
@@ -26,22 +26,24 @@ class CensusFipsArchiver(AbstractDatasetArchiver):
             if not matches:
                 continue
             year = int(matches.group(1))
-            # don't go peep into the 1990-2000/ dir which just includes a txt file
-            if self.valid_year(year) and year != 2000:
+            if self.valid_year(year):
                 yield self.get_year_resource(year)
 
     async def get_year_resource(self, year: int) -> tuple[Path, dict]:
-        """Download excel file."""
+        """Download excel or txt file."""
         # before 2017, the files are xls. after that its xlsx
-        link_url = f"{BASE_URL}/{year}"
-        link_pattern = re.compile(rf"all-geocodes-v{year}.(xlsx|xls)")
-        file_names = await self.get_hyperlinks(link_url, link_pattern)
-        if len(file_names) != 1:
-            raise AssertionError(
-                f"We expected exactly one link for {year}, but we found: {file_names}"
-            )
-        file_name = file_names.pop()
+        if year != 2000:
+            link_url = f"{BASE_URL}/{year}"
+            link_pattern = re.compile(rf"all-geocodes-v{year}.(xlsx|xls)")
+            file_names = await self.get_hyperlinks(link_url, link_pattern)
+            if len(file_names) != 1:
+                raise AssertionError(
+                    f"We expected exactly one link for {year}, but we found: {file_names}"
+                )
+            file_name = file_names.pop()
+        elif year == 2000:
+            link_url = f"{BASE_URL}/1990-2000"
+            file_name = "90s-fips.txt"
         download_path = self.download_directory / file_name
         await self.download_file(f"{link_url}/{file_name}", download_path)
-
         return ResourceInfo(local_path=download_path, partitions={"year": year})
