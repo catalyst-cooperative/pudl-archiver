ae81e6e2c7060196e2d0fc553231b741560a7ac7
Christina Gosnell <cgosnell@catalyst.coop>
2025-01-17 15:33:23 -0500

    Add EIA CBECS metadata

--- src/pudl_archiver/metadata/sources.py
+++ src/pudl_archiver/metadata/sources.py
@@ -22,6 +22,43 @@ from pudl.metadata.constants import CONTRIBUTORS, LICENSES
 ADDL_CONTRIBUTORS: dict[str, dict[str, str]] = {}
 
 NON_PUDL_SOURCES: dict[str, Any] = {
+    "eiacbecs": {
+        "title": "EIA CBECS -- Commercial Buildings Energy Consumption Survey",
+        "path": "https://www.eia.gov/consumption/manufacturing/data/2018/",
+        "description": (
+            "The Commercial Buildings Energy Consumption Survey (CBECS) is a national"
+            "sample survey that collects information on the stock of U.S. commercial"
+            "buildings, including their energy-related building characteristics and"
+            "energy usage data (consumption and expenditures). Commercial buildings"
+            "include all buildings in which at least half of the floorspace is used for"
+            "a purpose that is not residential, industrial, or agricultural. By this"
+            "definition, CBECS includes building types that might not traditionally be"
+            "considered commercial, such as schools, hospitals, correctional"
+            "institutions, and buildings used for religious worship, in addition to"
+            "traditional commercial buildings such as stores, restaurants, warehouses,"
+            "and office buildings."
+        ),
+        "working_partitions": {
+            "years": [
+                2003,
+                2012,
+                2018,
+            ]  # there are PDF only versions for 1999, 1995 and 1992
+        },
+        "keywords": sorted(
+            {
+                "eia",
+                "energy",
+                "cbecs",
+                "consumption",
+                "buildings",
+                "commercial",
+            }
+        ),
+        "license_raw": LICENSES["us-govt"],
+        "license_pudl": LICENSES["cc-by-4.0"],
+        "contributors": [CONTRIBUTORS["catalyst-cooperative"]],
+    },
     "eiamecs": {
         "title": "EIA MECS -- Manufacturing Energy Consumption Survey",
         "path": "https://www.eia.gov/consumption/manufacturing/data/2018/",
