8fe90a1cfea959ec354b6dd719584d7ea45195a6
e-belfer <ella.belfer@catalyst.coop>
2025-01-24 16:12:15 -0500

    Add production concept DOI, add to github archiving workflow

--- src/pudl_archiver/archivers/usgsuspvdb.py
+++ src/pudl_archiver/archivers/usgsuspvdb.py
@@ -24,8 +24,8 @@ class UsgsUsPvDbArchiver(AbstractDatasetArchiver):
     at https://energy.usgs.gov/uspvdb/data/" but the filename will include the date of the release
     which is not predictable.
 
-    This code will have to be updated if new versions are available. Maybe check for the number of
-    datasets returned in the catalog somehow?
+    This code will have to be updated if new versions are available. It will raise an
+    error if a version is found that isn't already mapped.
     """
 
     name = "usgsuspvdb"
@@ -36,32 +36,34 @@ class UsgsUsPvDbArchiver(AbstractDatasetArchiver):
         link_pattern = re.compile(r"\/item\/(\w+)$")
         for link in await self.get_hyperlinks(BASE_URL, link_pattern):
             dataset_id = link_pattern.search(link).group(1)
-            yield self.get_crosswalk_zip(link, dataset_id)
+            yield self.get_version_resource(link, dataset_id)
 
-    async def get_crosswalk_zip(self, link: str, dataset_id: str) -> tuple[Path, dict]:
-        """Download entire dataset as a zipfile.
+    async def get_version_resource(
+        self, link: str, dataset_id: str
+    ) -> tuple[Path, dict]:
+        """Download entire dataset as a zipfile for a given year.
 
         The `get` URLs are found on:
         * https://www.sciencebase.gov/catalog/item/6442d8a2d34ee8d4ade8e6db
         * https://www.sciencebase.gov/catalog/item/6671c479d34e84915adb7536
 
-        The date on the file is the date of publication, while we want the date of the
-        partition to correspond to data availability. For simplicity, we map this manually
-        and alert ourselves when any new data is published.
+        The date on the file is the date of publication, and the time range covered
+        by the dataset varies from version to version. To keep things simple, we map
+        urls to their version manually and alert ourselves when any new data is published.
         """
         crosswalk_urls = {
-            "6442d8a2d34ee8d4ade8e6db": 2021,
-            "6671c479d34e84915adb7536": 2023,
+            "6442d8a2d34ee8d4ade8e6db": "1.0",
+            "6671c479d34e84915adb7536": "2.0",
         }
         try:
-            year = crosswalk_urls[dataset_id]
+            version = crosswalk_urls[dataset_id]
         except KeyError:
             raise KeyError(
-                f"Dataset ID {dataset_id} at link {link} isn't mapped to a year in crosswalk_urls. Is this a new year of data?"
+                f"Dataset ID {dataset_id} at link {link} isn't mapped to a version in crosswalk_urls. Is this a new version of data?"
             )
-        download_path = self.download_directory / f"usgsuspvdb-{year}.zip"
+        download_path = self.download_directory / f"usgsuspvdb-{version}.zip"
         download_link = f"https://www.sciencebase.gov/catalog/file/get/{dataset_id}"
 
         await self.download_zipfile(download_link, download_path)
 
-        return ResourceInfo(local_path=download_path, partitions={"year": year})
+        return ResourceInfo(local_path=download_path, partitions={"version": version})
