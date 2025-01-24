44ea29207e8c921d86318442edd7dbf5fbc2b43a
Christina Gosnell <cgosnell@catalyst.coop>
2025-01-24 11:50:08 -0500

    wip draft of multi-year mecs

--- src/pudl_archiver/archivers/eia/eiamecs.py
+++ src/pudl_archiver/archivers/eia/eiamecs.py
@@ -8,6 +8,7 @@ from pudl_archiver.archivers.classes import (
     ArchiveAwaitable,
     ResourceInfo,
 )
+from pudl_archiver.frictionless import ZipLayout
 
 BASE_URL = "https://www.eia.gov/consumption/manufacturing/data"
 logger = logging.getLogger(f"catalystcoop.{__name__}")
@@ -20,16 +21,22 @@ class EiaMECSArchiver(AbstractDatasetArchiver):
 
     async def get_resources(self) -> ArchiveAwaitable:
         """Download EIA-MECS resources."""
-        for year in [2018]:
+        years_url = "https://www.eia.gov/consumption/data.php#mfg"
+        year_link_pattern = re.compile(r"(manufacturing/data/)(\d{4})/$")
+        for link in await self.get_hyperlinks(years_url, year_link_pattern):
+            match = year_link_pattern.search(link)
+            year = match.groups()[1]
             yield self.get_year_resources(year)
 
     async def get_year_resources(self, year: int) -> list[ResourceInfo]:
         """Download all excel tables for a year."""
+        logger.info(f"Attempting to find resources for: {year}")
         table_link_pattern = re.compile(r"[Tt]able(\d{1,2})_(\d{1,2}).xlsx")
 
         # Loop through all download links for tables
-        tables = []
+        data_paths_in_archive = set()
         year_url = f"{BASE_URL}/{year}"
+        zip_path = self.download_directory / f"eiamecs-{year}.zip"
         for table_link in await self.get_hyperlinks(year_url, table_link_pattern):
             table_link = f"{year_url}/{table_link}"
             logger.info(f"Fetching {table_link}")
@@ -38,16 +45,20 @@ class EiaMECSArchiver(AbstractDatasetArchiver):
             major_num, minor_num = match.group(1), match.group(2)
 
             # Download file
-            download_path = (
-                self.download_directory
-                / f"eia-mecs-{year}-table-{major_num}-{minor_num}.xlsx"
+            filename = f"eia-mecs-{year}-table-{major_num}-{minor_num}.xlsx"
+            download_path = self.download_directory / filename
+            await self.download_file(table_link, download_path)
+            self.add_to_archive(
+                zip_path=zip_path,
+                filename=filename,
+                blob=download_path.open("rb"),
             )
-            await self.download_zipfile(table_link, download_path)
-
-            tables.append(
-                ResourceInfo(
-                    local_path=download_path,
-                    partitions={"year": year, "table": f"{major_num}_{minor_num}"},
-                )
-            )
-        return tables
+            data_paths_in_archive.add(filename)
+            # Don't want to leave multiple giant CSVs on disk, so delete
+            # immediately after they're safely stored in the ZIP
+            download_path.unlink()
+        return ResourceInfo(
+            local_path=zip_path,
+            partitions={"year": year},
+            layout=ZipLayout(file_paths=data_paths_in_archive),
+        )
