e062310e3b4a8f158ae7fc910ffef746cfcfb2ad
e-belfer <ella.belfer@catalyst.coop>
2025-01-24 12:43:40 -0500

    Grab dataset IDs from catalog and add an assertion for new years of data

--- src/pudl_archiver/archivers/usgsuspvdb.py
+++ src/pudl_archiver/archivers/usgsuspvdb.py
@@ -1,5 +1,6 @@
 """Download USGS USPVDB -- U.S. Large-Scale Solar Photovoltaic Database."""
 
+import re
 from pathlib import Path
 
 from pudl_archiver.archivers.classes import (
@@ -7,6 +8,8 @@ from pudl_archiver.archivers.classes import (
     ArchiveAwaitable,
     ResourceInfo,
 )
+
+BASE_URL = "https://www.sciencebase.gov/catalog/items?parentId=66707f69d34e89718fa3f82f"
 
 
 class UsgsUsPvDbArchiver(AbstractDatasetArchiver):
@@ -29,21 +32,36 @@ class UsgsUsPvDbArchiver(AbstractDatasetArchiver):
 
     async def get_resources(self) -> ArchiveAwaitable:
         """Download the 2 version of the database released in different years."""
-        for year in [2023, 2024]:
-            yield self.get_crosswalk_zip(year)
+        # Get any link matching /item/alphanumeric and capture the alphanumeric part
+        link_pattern = re.compile(r"\/item\/(\w+)$")
+        for link in await self.get_hyperlinks(BASE_URL, link_pattern):
+            dataset_id = link_pattern.search(link).group(1)
+            yield self.get_crosswalk_zip(link, dataset_id)
 
-    async def get_crosswalk_zip(self, year: int) -> tuple[Path, dict]:
+    async def get_crosswalk_zip(self, link: str, dataset_id: str) -> tuple[Path, dict]:
         """Download entire dataset as a zipfile.
 
         The `get` URLs are found on:
         * https://www.sciencebase.gov/catalog/item/6442d8a2d34ee8d4ade8e6db
         * https://www.sciencebase.gov/catalog/item/6671c479d34e84915adb7536
+
+        The date on the file is the date of publication, while we want the date of the
+        partition to correspond to data availability. For simplicity, we map this manually
+        and alert ourselves when any new data is published.
         """
         crosswalk_urls = {
-            2023: "https://www.sciencebase.gov/catalog/file/get/6442d8a2d34ee8d4ade8e6db",
-            2024: "https://www.sciencebase.gov/catalog/file/get/6671c479d34e84915adb7536",
+            "6442d8a2d34ee8d4ade8e6db": 2021,
+            "6671c479d34e84915adb7536": 2023,
         }
-        download_path = self.download_directory / f"usgsuspvdb_{year}.zip"
-        await self.download_zipfile(crosswalk_urls[year], download_path)
+        try:
+            year = crosswalk_urls[dataset_id]
+        except KeyError:
+            raise KeyError(
+                f"Dataset ID {dataset_id} at link {link} isn't mapped to a year in crosswalk_urls. Is this a new year of data?"
+            )
+        download_path = self.download_directory / f"usgsuspvdb-{year}.zip"
+        download_link = f"https://www.sciencebase.gov/catalog/file/get/{dataset_id}"
+
+        await self.download_zipfile(download_link, download_path)
 
         return ResourceInfo(local_path=download_path, partitions={"year": year})
