f06f7e3b938de545a80460cdf0d62329687813ff
Dazhong Xia <dazhong.xia@catalyst.coop>
2025-01-29 18:40:59 -0500

    feat: add 2015
    2015 methodology required some changes to allow for downloading html files.

--- src/pudl_archiver/archivers/eia/eiarecs.py
+++ src/pudl_archiver/archivers/eia/eiarecs.py
@@ -21,9 +21,11 @@ class LinkSet:
     See https://www.eia.gov/consumption/residential/data/2020/.
     """
 
-    url: str
+    view: str
     short_name: str
+    extension: str
     pattern: re.Pattern
+    skip_if_html: bool = True
 
 
 def _url_for(year: int, view: str):
@@ -36,36 +38,81 @@ def _url_for(year: int, view: str):
 YEAR_LINK_SETS = {
     2020: {
         "housing_characteristics": LinkSet(
-            url=_url_for(year=2020, view="characteristics"),
+            view="characteristics",
             short_name="hc",
-            pattern=re.compile(r"HC (\d{1,2}\.\d{1,2})\.(xlsx)"),
+            pattern=re.compile(r"HC (\d{1,2}\.\d{1,2})\.xlsx"),
+            extension="xlsx",
         ),
         "consumption & expenditures": LinkSet(
-            url=_url_for(year=2020, view="consumption"),
+            view="consumption",
             short_name="ce",
-            pattern=re.compile(r"ce(\d\.\d{1,2}[a-z]?)\.(xlsx)"),
+            pattern=re.compile(r"ce(\d\.\d{1,2}[a-z]?)\.xlsx"),
+            extension="xlsx",
         ),
         "state data (housing characteristics)": LinkSet(
-            url=_url_for(year=2020, view="state"),
-            short_name="state",
-            pattern=re.compile(r"State (.*)\.(xlsx)"),
+            view="state",
+            short_name="state-hc",
+            pattern=re.compile(r"State (.*)\.xlsx"),
+            extension="xlsx",
         ),
         "state data (consumption & expenditures)": LinkSet(
-            url=_url_for(year=2020, view="state"),
+            view="state",
             short_name="state-ce",
-            pattern=re.compile(r"ce(\d\.\d{1,2}\..*)\.(xlsx)"),
+            pattern=re.compile(r"ce(\d\.\d{1,2}\..*)\.xlsx"),
+            extension="xlsx",
         ),
         "microdata": LinkSet(
-            url=_url_for(year=2020, view="microdata"),
+            view="microdata",
             short_name="microdata",
-            pattern=re.compile(r"(recs.*public.*)\.(csv)"),
+            pattern=re.compile(r"(recs.*public.*)\.csv"),
+            extension="csv",
+        ),
+        "microdata-codebook": LinkSet(
+            view="microdata",
+            short_name="microdata",
+            pattern=re.compile(r"(RECS 2020 Codebook.*v.)\.xlsx"),
+            extension="xlsx",
         ),
         "methodology": LinkSet(
-            url=_url_for(year=2020, view="methodology"),
+            view="methodology",
             short_name="methodology",
-            pattern=re.compile(r"pdf/(.+)\.(pdf)"),
+            pattern=re.compile(r"pdf/(.+)\.pdf"),
+            extension="pdf",
         ),
-    }
+    },
+    2015: {
+        "housing_characteristics": LinkSet(
+            view="characteristics",
+            short_name="hc",
+            pattern=re.compile(r"hc(\d{1,2}\.\d{1,2})\.xlsx"),
+            extension="xlsx",
+        ),
+        "consumption & expenditures": LinkSet(
+            view="consumption",
+            short_name="ce",
+            pattern=re.compile(r"ce(\d\.\d{1,2}[a-z]?)\.xlsx"),
+            extension="xlsx",
+        ),
+        "microdata": LinkSet(
+            view="microdata",
+            short_name="microdata",
+            pattern=re.compile(r"(recs.*public.*)\.csv"),
+            extension="csv",
+        ),
+        "microdata-codebook": LinkSet(
+            view="microdata",
+            short_name="microdata",
+            pattern=re.compile(r"(codebook.*)\.xlsx"),
+            extension="xlsx",
+        ),
+        "methodology": LinkSet(
+            view="methodology",
+            short_name="methodology",
+            pattern=re.compile(r"/consumption/residential/reports/2015/(.+)(\.php)?"),
+            extension="html",
+            skip_if_html=False,
+        ),
+    },
 }
 logger = logging.getLogger(f"catalystcoop.{__name__}")
 
@@ -77,13 +124,13 @@ class EiaRECSArchiver(AbstractDatasetArchiver):
 
     async def get_resources(self) -> ArchiveAwaitable:
         """Download EIA-RECS resources."""
-        for year in [2020]:
+        for year in [2020, 2015]:
             yield self.get_year_resources(year)
 
     def __is_html_file(self, fileobj: BytesIO) -> bool:
         header = fileobj.read(30).lower().strip()
         fileobj.seek(0)
-        return b"<!doctype html>" in header
+        return b"<!doctype html" in header
 
     async def get_year_resources(self, year: int) -> list[ResourceInfo]:
         """Download all excel tables for a year."""
@@ -94,21 +141,25 @@ class EiaRECSArchiver(AbstractDatasetArchiver):
         # Loop through different categories of data (all .xlsx)
         link_sets = YEAR_LINK_SETS[year]
         for link_set in link_sets.values():
-            for table_link in await self.get_hyperlinks(link_set.url, link_set.pattern):
-                table_link = urljoin(link_set.url, table_link)
+            url = _url_for(year, link_set.view)
+            for table_link in await self.get_hyperlinks(url, link_set.pattern):
+                table_link = urljoin(url, table_link).strip("/")
                 logger.info(f"Fetching {table_link}")
                 match = link_set.pattern.search(table_link)
-                matched_metadata = (
-                    match.group(1).replace(".", "-").replace(" ", "_").lower()
+                matched_filename = (
+                    match.group(1)
+                    .replace(".", "-")
+                    .replace(" ", "_")
+                    .replace("/", "-")
+                    .lower()
                 )
-                matched_format = match.group(2)
-                output_filename = f"eia-recs-{year}-{link_set.short_name}-{matched_metadata}.{matched_format}"
+                output_filename = f"eia-recs-{year}-{link_set.short_name}-{matched_filename}.{link_set.extension}"
 
                 # Download file
                 download_path = self.download_directory / output_filename
                 await self.download_file(table_link, download_path)
                 with download_path.open("rb") as f:
-                    if self.__is_html_file(f):
+                    if link_set.skip_if_html and self.__is_html_file(f):
                         continue
                     self.add_to_archive(
                         zip_path=zip_path,
