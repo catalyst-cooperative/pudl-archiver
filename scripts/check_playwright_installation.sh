9804772cf503ef684fe2d085fc8c269c8f5a3a12
Kathryn Mazaitis <kathryn.mazaitis@catalyst.coop>
2025-06-11 16:50:31 -0400

    More options for installing playwright dependencies

--- scripts/check_playwright_installation.sh
+++ scripts/check_playwright_installation.sh
@@ -2,4 +2,4 @@
 
 install_path=`playwright install webkit --dry-run |grep "Install location" |sed 's/^[ ]*Install location:[ ]*//'`
 if [ -f "$install_path" ]; then exit 0; fi
-playwright install webkit
+playwright install --with-deps webkit
