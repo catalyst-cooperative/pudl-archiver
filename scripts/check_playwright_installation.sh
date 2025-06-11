6f075ecbcc01c64ca7545828bbc3821e32eaa01d
Kathryn Mazaitis <kathryn.mazaitis@catalyst.coop>
2025-06-11 16:29:38 -0400

    Add pixi setup for playwright

---
+++ scripts/check_playwright_installation.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+install_path=`playwright install webkit --dry-run |grep "Install location" |sed 's/^[ ]*Install location:[ ]*//'`
+if [ -f "$install_path" ]; then exit 0; fi
+playwright install webkit
