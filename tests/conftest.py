2707c653c4e202c4c68125bcbb339696d38a4ecd
zschira <zach.schira@catalyst.coop>
2025-08-27 13:15:02 -0400

    Test fix to unit test for CI

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,5 @@
 """Pytest configuration module."""
 
-import tempfile
 import zipfile
 from datetime import datetime
 from pathlib import Path
@@ -28,39 +27,36 @@ def datapackage():
 
 
 @pytest.fixture()
-def bad_zipfile():
+def bad_zipfile(tmp_path):
     """Create a fake bad zipfile as a temp file."""
-    with tempfile.TemporaryDirectory() as path:
-        zip_path = Path(path) / "bad.zip"
-        with Path.open(zip_path, "wb") as archive:
-            archive.write(b"Fake non-zipfile data")
+    zip_path = tmp_path / "bad.zip"
+    with Path.open(zip_path, "wb") as archive:
+        archive.write(b"Fake non-zipfile data")
 
-        yield zip_path
+    yield zip_path
 
 
 @pytest.fixture()
-def fixed_bad_zipfile():
+def fixed_bad_zipfile(tmp_path):
     """Create a fixed version of 'bad.zip' as a temp file."""
-    with tempfile.TemporaryDirectory() as path:
-        zip_path = Path(path) / "bad.zip"
-        with (
-            zipfile.ZipFile(zip_path, "w") as archive,
-            archive.open("test.txt", "w") as file,
-        ):
-            file.write(b"Test fixed bad zipfile")
+    zip_path = tmp_path / "bad.zip"
+    with (
+        zipfile.ZipFile(zip_path, "w") as archive,
+        archive.open("test.txt", "w") as file,
+    ):
+        file.write(b"Test fixed bad zipfile")
 
-        yield zip_path
+    yield zip_path
 
 
 @pytest.fixture()
-def good_zipfile():
+def good_zipfile(tmp_path):
     """Create a fake good zipfile in temporary directory."""
-    with tempfile.TemporaryDirectory() as path:
-        zip_path = Path(path) / "good.zip"
-        with (
-            zipfile.ZipFile(zip_path, "w") as archive,
-            archive.open("test.txt", "w") as file,
-        ):
-            file.write(b"Test good zipfile")
+    zip_path = tmp_path / "good.zip"
+    with (
+        zipfile.ZipFile(zip_path, "w") as archive,
+        archive.open("test.txt", "w") as file,
+    ):
+        file.write(b"Test good zipfile")
 
-        yield zip_path
+    yield zip_path
