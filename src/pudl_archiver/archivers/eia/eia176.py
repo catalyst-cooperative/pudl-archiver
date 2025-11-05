3cdcdf1e4bd6995b46426902804056d7fadf1e48
Ella Belfer <e.belfer@berkeley.edu>
2025-11-05 13:52:49 -0500

    Rename to clarify different components, add docstrings, drop empty company list files

--- src/pudl_archiver/archivers/eia/eia176.py
+++ src/pudl_archiver/archivers/eia/eia176.py
@@ -1,8 +1,22 @@
 """Download EIA 176 data.
 
-Unlike the other NGQV datasets, the "reports" provided for EIA 176 do not contain all
-fields of data. To get around this, we query the list of item codes for 176 from the
-portal, and then manually download subsets of data from all item codes for all years.
+EIA 176 data is contained in three formats:
+1. A bulk zipfile download (which also contains some EIA 191 data). As of 11/2025, this
+    should contain all data but has some missing data relative to the reports that is
+    not well explained.
+2. Pre-defined reports which contain different slices of the EIA 176 survey data.
+3. A custom report, which allows you to download a larger range of variables than are
+    contained in the pre-defined reports. This includes all numeric data, but doesn't
+    include absolutely everything found in the bulkfile (non-numeric variables aren't
+    included.)
+
+Unlike the other NGQV datasets (EIA 191 and 757A), the "reports" provided for EIA 176 do
+not contain all fields of data. To get around this, we query the list of variable codes
+for 176 from the portal, and then manually download subsets of data from all variable
+codes for all years.
+
+By archiving all three formats, we make sure we're capturing subtle variations in
+data content across each format.
 """
 
 import asyncio
@@ -25,15 +39,19 @@ class Eia176Archiver(EiaNGQVArchiver):
     name = "eia176"
     form = "176"
     data_url = "https://www.eia.gov/naturalgas/ngqs/data/report/RPC/data/"
-    items_url = "https://www.eia.gov/naturalgas/ngqs/data/items"
+    variables_url = "https://www.eia.gov/naturalgas/ngqs/data/items"
     bulk_url = "https://www.eia.gov/naturalgas/ngqs/all_ng_data.zip"
 
-    async def get_items(self, url: str = items_url) -> list[str]:
-        """Get list of item codes from EIA NQGV portal."""
-        self.logger.info("Getting list of items in Form 176.")
-        items_response = await self.get_json(url)
-        items_list = [item["item"] for item in items_response]
-        return items_list
+    async def get_variables(self, url: str = variables_url) -> list[str]:
+        """Get list of variable codes from EIA NQGV portal.
+
+        Each variable code corresponds to a variable that we can download from the custom
+        report for EIA 176. We get all the variable codes
+        """
+        self.logger.info("Getting list of all variables in Form 176.")
+        variables_response = await self.get_json(url)
+        variables_list = [variable["item"] for variable in variables_response]
+        return variables_list
 
     async def get_resources(self) -> ArchiveAwaitable:
         """Download EIA 176 resources, including all fields from the custom report."""
@@ -41,7 +59,13 @@ class Eia176Archiver(EiaNGQVArchiver):
         yield self.get_bulk_resource()
 
         # Then archive the already-defined reports
-        reports_list = await self.get_datasets(url=self.base_url, form=self.form)
+        reports_list = await self.get_reports(url=self.base_url, form=self.form)
+
+        # Check that we don't have any unexpected reports here
+        report_codes = {report.report_code for report in reports_list}
+        assert report_codes == {"RP1", "RP2", "RP3", "RP4", "RP5", "RP6", "RPC"}, (
+            f"Got unexpected reports for 176: {report_codes}."
+        )
 
         # For the custom fields, we want to ensure we're querying all possible
         # years of data availability across all EIA forms.
@@ -51,16 +75,16 @@ class Eia176Archiver(EiaNGQVArchiver):
 
         for report in reports_list:
             # Get all available years
-            if report.code != "RPC":  # We handle the custom report separately
+            if report.report_code != "RPC":  # We handle the custom report separately
                 report_years = [year.ayear for year in report.available_years]
                 all_report_years.update(report_years)  # Update the global list of years
 
         # Now, grab the data from the custom report
-        items_list = await self.get_items(url=self.items_url)
+        variables_list = await self.get_variables(url=self.variables_url)
 
         for year in all_report_years:
             yield self.get_year_reports_and_custom_resource(
-                str(year), reports_list, items_list
+                str(year), reports_list, variables_list
             )
 
     async def get_bulk_resource(self) -> ResourceInfo:
@@ -76,19 +100,25 @@ class Eia176Archiver(EiaNGQVArchiver):
         """Define partitions for year resource. Override to handle complex partitions."""
         return {"year": year, "format": "by_report"}
 
-    async def download_all_custom_fields(self, year: str, items_list: list[str]):
-        """Download all custom items from the EIA 176 custom report to a CSV."""
+    async def download_all_custom_fields(self, year: str, variables_list: list[str]):
+        """Download all custom variables from the EIA 176 custom report to a CSV.
+
+        Args:
+            year: year of the partition
+            variables_list: A list containing the shortcodes for all variables in the
+                custom form.
+        """
         csv_name = f"eia176_{year}_custom.csv"
         dataframes = []
 
-        for i in range(0, len(items_list), 20):
-            self.logger.debug(f"Getting items {i}-{i + 20} of data for {year}")
-            # Chunk items list into 20 to avoid error message
+        for i in range(0, len(variables_list), 20):
+            self.logger.debug(f"Getting variables {i}-{i + 20} of data for {year}")
+            # Chunk variables list into 20 to avoid error message
             download_url = self.data_url + f"{year}/{year}/ICA/Name/"
-            items = items_list[i : i + 20]
-            for item in items:
-                download_url += f"{item}/"
-            download_url = download_url[:-1]  # Drop trailing slash
+            variables = variables_list[i : i + 20]
+            for variable in variables:
+                download_url += f"{variable}/"
+            download_url = download_url.rstrip("/")  # Drop trailing slash
 
             user_agent = self.get_user_agent()
             json_response = await self.get_json(
@@ -110,12 +140,12 @@ class Eia176Archiver(EiaNGQVArchiver):
 
         # Rename columns. Instead of using year for value column, rename "value"
         column_dict = {
-            item["field"]: (
-                str(item["headerName"]).lower()
-                if str(item["headerName"]).lower() != year
+            variable["field"]: (
+                str(variable["headerName"]).lower()
+                if str(variable["headerName"]).lower() != year
                 else "value"
             )
-            for item in json_response["columns"]
+            for variable in json_response["columns"]
         }
         dataframe = dataframe.rename(columns=column_dict).sort_values(
             ["company", "item"]
@@ -129,7 +159,10 @@ class Eia176Archiver(EiaNGQVArchiver):
         return csv_name, csv_data
 
     async def get_year_reports_and_custom_resource(
-        self, year: str, reports_list: list[EIANaturalGasData], items_list: list[str]
+        self,
+        year: str,
+        reports_list: list[EIANaturalGasData],
+        variables_list: list[str],
     ) -> ResourceInfo:
         """Download all available data for a year with multiple reports.
 
@@ -139,7 +172,10 @@ class Eia176Archiver(EiaNGQVArchiver):
 
         Args:
             year: the year we're downloading data for
-            report: the report we're downloading
+            reports_list: a list containing metadata items for each report to be
+                downloaded
+            variables_list: a list containing the shortcodes for all the variables
+                in the custom form.
         """
         archive_path = self.download_directory / f"{self.name}-{year}.zip"
         data_paths_in_archive = set()
@@ -149,7 +185,7 @@ class Eia176Archiver(EiaNGQVArchiver):
             # and skipping the custom data form
             if (
                 int(year) in [year.ayear for year in report.available_years]
-                and report.code != "RPC"
+                and report.report_code != "RPC"
             ):
                 # Get a string interpretable name for the report
                 report_name = (
@@ -158,31 +194,35 @@ class Eia176Archiver(EiaNGQVArchiver):
                 csv_name = f"{self.name}_{year}{report_name}.csv"
 
                 download_url = (
-                    self.base_url + f"/{report.code}/data/{year}/{year}/ICA/Name"
+                    self.base_url + f"/{report.report_code}/data/{year}/{year}/ICA/Name"
                 )
 
-                self.logger.info(f"Retrieving data for {year} {report.code}")
+                self.logger.info(f"Retrieving data for {year} {report.report_code}")
                 json_response = await self.get_json(download_url)
                 dataframe = pd.DataFrame.from_dict(
                     json_response["data"], orient="columns"
                 )
 
+                if dataframe.empty and report.report_code == "RP6":
+                    # The company list is expected to not have data in a known number of years.
+                    # Skip uploading an empty file, but warn us if other years don't
+                    # have data.
+                    assert year in ["2000", "2001", "2019"]
+                    self.logger.warn(f"No data found for {year} {report.report_code}")
+                    continue
+
                 # Rename columns
                 column_dict = {
-                    item["field"]: str(item["headerName"])
-                    .lower()
-                    .replace("<br>", "_")
-                    .replace(" ", "_")
-                    for item in json_response["columns"]
-                    if "children" not in item
+                    variable["field"]: self.clean_header_name(
+                        str(variable["headerName"])
+                    )
+                    for variable in json_response["columns"]
+                    if "children" not in variable
                 } | {  # Handle children
-                    child["field"]: str(child["headerName"])
-                    .lower()
-                    .replace("<br>", "_")
-                    .replace(" ", "_")
-                    for item in json_response["columns"]
-                    if "children" in item
-                    for child in item["children"]
+                    child["field"]: self.clean_header_name(str(child["headerName"]))
+                    for variable in json_response["columns"]
+                    if "children" in variable
+                    for child in variable["children"]
                 }
                 dataframe = dataframe.rename(columns=column_dict)
 
@@ -203,7 +243,7 @@ class Eia176Archiver(EiaNGQVArchiver):
                 await asyncio.sleep(5)  # Avoid getting cut off
 
         # Now handle custom form data
-        csv_name, csv_data = await self.download_all_custom_fields(year, items_list)
+        csv_name, csv_data = await self.download_all_custom_fields(year, variables_list)
 
         with zipfile.ZipFile(
             archive_path,
