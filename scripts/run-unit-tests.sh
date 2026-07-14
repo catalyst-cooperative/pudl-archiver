f5ab8bc62fc89f32fd060c0c27c2a2f3ddb053bb
zschira <zach.schira@catalyst.coop>
2026-07-14 14:09:20 -0400

    Document docker dev workflow

--- scripts/run-unit-tests.sh
+++ scripts/run-unit-tests.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 if [[ "${PUDL_ARCHIVER_USE_DOCKER:-0}" == "true" ]]; then
     echo "Running tests in Docker..."
-    docker compose run --rm pudl-archiver-pytest tests/unit
+    docker compose run --rm pudl_archiver_pytest tests/unit
 else
     echo "Running tests with local Pixi..."
     pixi run pytest tests/unit
