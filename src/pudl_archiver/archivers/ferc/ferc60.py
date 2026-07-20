4d638afdd7ed606217a44aac6d046eb8afe1a354
zschira <zach.schira@catalyst.coop>
2026-07-20 12:27:28 -0400

    Remove unused code

--- src/pudl_archiver/archivers/ferc/ferc60.py
+++ src/pudl_archiver/archivers/ferc/ferc60.py
@@ -1,11 +1,8 @@
 """Defines base class for archiver."""
 
-from pathlib import Path
-
 from pudl_archiver.archivers.classes import (
     AbstractDatasetArchiver,
     ArchiveAwaitable,
-    ResourceInfo,
 )
 from pudl_archiver.archivers.ferc import ferc_online_helpers, xbrl
 
@@ -32,14 +29,4 @@ class Ferc60Archiver(AbstractDatasetArchiver):
             self.download_directory,
             self.valid_year,
             self.session,
-        )
-
-    async def get_year_dbf(self, year: int) -> tuple[Path, dict]:
-        """Download a single year of FERC Form 60 data."""
-        url = f"https://forms.ferc.gov/f60allyears/f60_{year}.zip"
-        download_path = self.download_directory / f"ferc60-{year}.zip"
-        await self.download_zipfile(url, download_path)
-
-        return ResourceInfo(
-            local_path=download_path, partitions={"year": year, "data_format": "dbf"}
         )
