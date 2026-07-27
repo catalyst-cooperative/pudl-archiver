5f241deec7637608ceff43bbef8975674eaaf0c4
e-belfer <ella.belfer@catalyst.coop>
2026-07-27 12:40:45 -0400

    Update to use github LFS for repository

--- src/pudl_archiver/archivers/eia/eiabluesky.py
+++ src/pudl_archiver/archivers/eia/eiabluesky.py
@@ -1,5 +1,7 @@
 """Download EIA Project BlueSky Github respository."""
 
+import os
+import subprocess
 from pathlib import Path
 
 from pudl_archiver.archivers.classes import (
@@ -7,8 +9,10 @@ from pudl_archiver.archivers.classes import (
     ArchiveAwaitable,
     ResourceInfo,
 )
+from pudl_archiver.frictionless import ZipLayout
 
 BASE_URL = "https://github.com/EIAgov/BlueSky/archive/refs/tags"
+EXPECTED_TAGS = ["v1.0", "v1.1"]  # Sanitize tags for subprocess calls
 
 
 class EIABlueSkyArchiver(AbstractDatasetArchiver):
@@ -20,22 +24,62 @@ class EIABlueSkyArchiver(AbstractDatasetArchiver):
     concurrency_limit = 1
 
     async def get_resources(self) -> ArchiveAwaitable:
-        """Download EIA Blue Sky resources."""
+        """Download EIA NEMS resources."""
         release_json = await self.get_json(
             "https://api.github.com/repos/EIAgov/BlueSky/releases"
         )
+
         release_tags = [release["tag_name"] for release in release_json]
+        # Error the archiver if there's a new release
+        # so we can update the archiver manually.
+        if any(tag not in EXPECTED_TAGS for tag in release_tags):
+            raise ValueError(
+                f"Unexpected release! Releases: {release_tags}. Investigate and update release_to_year_map to archive."
+            )
+
+        # Clone the entire project
+        os.chdir(self.download_directory)
+        subprocess.run(  # noqa:S603
+            [
+                "/usr/bin/git",
+                "clone",
+                "https://api.github.com/repos/EIAgov/BlueSky.git",
+            ],
+            shell=False,
+        )
+        os.chdir(self.download_directory / "BlueSky")
+        subprocess.run(["/usr/bin/git", "lfs", "fetch", "--all"], shell=False)
+
         for tag in release_tags:
-            yield self.get_release_resource(tag)
+            yield self.get_release_resource(tag=tag)
 
     async def get_release_resource(self, tag: str) -> tuple[Path, dict]:
         """Download entire repo as a zipfile from github from a tagged release.
 
-        A release is expected to correspond to a release version of the code repository.
+        A release is expected to correspond to a tagged release in the BlueSky
+        repository.
         """
         tag_file_name = tag.lower().replace(".", "-")
-        url = f"{BASE_URL}/{tag}.zip"
-        download_path = self.download_directory / f"eiabluesky-{tag_file_name}.zip"
-        await self.download_zipfile(url, download_path)
+        zip_path = self.download_directory / f"eiabluesky-{tag_file_name}.zip"
+        data_paths_in_archive = set()
 
-        return ResourceInfo(local_path=download_path, partitions={"release": tag})
+        subprocess.run(["/usr/bin/git", "checkout", tag], shell=False)  # noqa:S603
+        # We sanitize tag above using the assertion, so this should be ok.
+        subprocess.run(["/usr/bin/git", "lfs", "pull"], shell=False)
+
+        directory = (self.download_directory / "NEMS").resolve()
+
+        for entry in directory.rglob("*"):
+            if entry.is_file():
+                self.add_to_archive(
+                    zip_path=zip_path,
+                    filename=str(entry.relative_to(directory)),
+                    blob=entry.open("rb"),
+                )
+                data_paths_in_archive.add(entry.relative_to(directory))
+
+        return ResourceInfo(
+            local_path=zip_path,
+            partitions={"release": tag},
+            layout=ZipLayout(file_paths=data_paths_in_archive),
+        )
