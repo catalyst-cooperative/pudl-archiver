0bddf3fa121bbcf6159cfbbc46fa9c96bc262653
Ella Belfer <e.belfer@berkeley.edu>
2026-09-14 10:57:30 -0400

    Fix time condition

--- src/pudl_archiver/archivers/ferc/ferccid.py
+++ src/pudl_archiver/archivers/ferc/ferccid.py
@@ -54,10 +54,10 @@ class FercCIDArchiver(AbstractDatasetArchiver):
 
             await page.goto(page_url, timeout=1000)
             await expect(
-                page.get_by_text(re.compile(r"\d:\d{2} (AM|PM)"))
+                page.get_by_text(re.compile(r"\d\/\d\/\d{4} \d{1,2}:\d{2} (AM|PM)"))
             ).to_be_visible()
             last_update = await page.get_by_text(
-                re.compile(r"\d:\d{2} (AM|PM)")
+                re.compile(r"\d\/\d\/\d{4} \d{1,2}:\d{2} (AM|PM)")
             ).inner_text()  # Search for date
             return date_parser.parse(last_update)
 
